a1a1ecd482ff78d9f42a2c38b452baa46bf99dd5
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-07-25 20:16:44 -0400

    Added functionality for adding a department and started functionality for adding a role

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ function init()
             {
                 viewAllEmployees();
             }
+
+            else if (response.userChoice === "Add a department")
+            {
+                addDepartment();
+            }
+
+            else if (response.userChoice === "Add a role")
+            {
+                addRole();
+            }
         });
 }
 
@@ -71,4 +81,96 @@ function viewAllEmployees()
     // init();
 }
 
+function addDepartment()
+{
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please enter a name for the new department',
+                name: 'newDepartment',
+            },
+        ])
+        .then((response) => {
+            console.log(response);
+
+            db.query('INSERT INTO department (name) VALUES (?)', response.newDepartment, (err, results) => {
+                if (err)
+                {
+                    console.error(err);
+                }
+                console.log(results);
+            })
+        });
+
+    // init();
+}
+
+function addRole()
+{
+    let params = [];
+
+    const departments = [];
+
+    db.query('SELECT * FROM department', function (err, results) {
+        
+        for (let i = 0; i < results.length; i++)
+        {
+            departments.push(results[i].name);
+        }
+
+        console.log(departments);
+    });
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of the new role',
+                name: 'name',
+            },
+
+            {
+                type: 'input',
+                message: 'Please enter the salary for the new role',
+                name: 'salary',
+            },
+
+            {
+                type: 'list',
+                message: 'Please select the department the role will be a part of',
+                choices: departments,
+                name: 'department',
+            },
+        ])
+        .then((response) => {
+            console.log(response);
+
+            let departmentID;
+
+            db.query('SELECT id FROM department WHERE name = ?', response.department, function (err, results) {
+                departmentID = results[0].id;
+
+                params = [response.name, response.salary, departmentID];
+
+                console.log(params);
+            });
+        })
+        .then(() => {
+
+            db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', params, function (err, results) {
+                if (err)
+                {
+                    console.log(err);
+                }
+
+                console.log(results);
+            });
+        });
+}
+
 init();
+
+// To Do
+
+// Fix SQL syntax error
